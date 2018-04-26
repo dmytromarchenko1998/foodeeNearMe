6fccc83eb318ef83009d6dfad4d2878bd19d2a86
Dmytro Marchenko <dmytromarchenko1998@gmail.com>
2018-04-26 14:05:30 -0700

    docker

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3005;
-const Business = require('./db/Business.js');
+const Business = require('./db/Business.js').Business;
+
 const cors = require('cors');
 
 app.use(morgan('dev'));
@@ -14,8 +15,11 @@ app.use(morgan('dev'));
 app.use(cors({'origin':'*'}));
 app.get('/api/:id', (req, res) => {
   var business_id = req.url.split('/api/')[1];
+  console.log(business_id);
+  // var query = Business.find({business_id:'-a857YYdjzgOdOjFFRsRXQ'});
   var query = Business.find({business_id:business_id});
   query.exec((err, businesses) => {
+    console.log('daaaaaawg', businesses);
     var selectedBusiness = businesses[0];
     var categoryArr = businesses[0].categories;
     for (var i =0; i < categoryArr.length; i++) {
@@ -26,6 +30,7 @@ app.get('/api/:id', (req, res) => {
     }
     var query = Business.find({categories:{$in:categoryArr}});
     query.exec((err, businesses) => {
+      // console.log('digity-dawg', businesses)
       if (businesses.length > 3) {
         for (var i = 0; i < businesses.length; i++) {
           if (businesses[i].business_id === business_id) {
